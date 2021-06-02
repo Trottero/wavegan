7f0d4c4c355d004ca2d0f5c58b7ddb7d47407a24
Trottero <wrlxs@hotmail.com>
2021-06-02 15:51:01 +0200

    Add inference script

--- infer.py
+++ infer.py
@@ -1,60 +1,56 @@
 # %%
 
-# import tensorflow as tf
-# import numpy as np
-# import librosa
-# from IPython.display import display, Audio
-# import os
-
-# # Load the graph
-# tf.reset_default_graph()
-# saver = tf.train.import_meta_graph('train-techno\model.ckpt-21002.meta')
-# graph = tf.get_default_graph()
-# sess = tf.InteractiveSession()
-# saver.restore(sess, 'train-techno\model.ckpt-21002')
-
-# # Create 50 random latent vectors z
-# _z = (np.random.rand(64, 100) * 2.) - 1
-
-# # Synthesize G(z)
-# z = graph.get_tensor_by_name('random_uniform:0')
-# G_z = graph.get_tensor_by_name('G_z:0')
-# _G_z = sess.run(G_z, {z: _z})
-
-# # Play audio in notebook
-# # display(Audio(_G_z[0, :, 0], rate=16000))
-
-# os.makedirs('infer', exist_ok=True)
-# # for i in range(64):
-# display(Audio(_G_z, rate=16000))
-# # x = np.array(_G_z)
-# # librosa.output.write_wav(f'infer/sample-{1}.wav', x, 16000)
-
-# # import tensorflow as tf
-# # tf.reset_default_graph()
-
-# # saver = tf.train.import_meta_graph('infer.meta')
-# # graph = tf.get_default_graph()
-# # sess = tf.InteractiveSession()
-# # saver.restore(sess, 'model.ckpt-10000')
-
-# # z_n = graph.get_tensor_by_name('samp_z_n:0')
-# # _z = sess.run(graph.get_tensor_by_name('samp_z:0'), {z_n: 10})
-
-# # z = graph.get_tensor_by_name('G_z:0')
-# # _G_z = sess.run(graph.get_tensor_by_name('G_z:0'), {z: _z})
-# %%
+import argparse
+import os
+import librosa
+import numpy as np
+import time as time
+import librosa.output as lo
 import tensorflow as tf
-tf.reset_default_graph()
+print(tf.VERSION)
 
-saver = tf.train.import_meta_graph('train-techno\infer\infer.meta')
+parser = argparse.ArgumentParser()
+# 'train-techno\model.ckpt-72825'
+parser.add_argument('--checkpoint_name', '-c', type=str, required=True)
+
+# 'train-techno\infer\infer.meta'
+parser.add_argument('--infer_graph', '-ig', type=str, required=True)
+parser.add_argument('--samples', '-n', type=str, default=100)
+parser.add_argument('--human_friendly', type=bool, default=False)
+
+args = parser.parse_args()
+
+tf.reset_default_graph()
+saver = tf.train.import_meta_graph(args.infer_graph)
 graph = tf.get_default_graph()
 sess = tf.InteractiveSession()
-saver.restore(sess, 'train-techno\model.ckpt-21002')
 
-z_n = graph.get_tensor_by_name('samp_z_n:0')
-_z = sess.run(graph.get_tensor_by_name('samp_z:0'), {z_n: 10})
+try:
+    saver.restore(sess, args.checkpoint_name)
 
-z = graph.get_tensor_by_name('G_z:0')
-_G_z = sess.run(graph.get_tensor_by_name('G_z:0'), {z: _z})
-print('')
+    # Sample latent vectors
+    _z = (np.random.rand(args.samples, 100) * 2.) - 1.
+
+    # Generate
+    z = graph.get_tensor_by_name('z:0')
+    G_z = graph.get_tensor_by_name('G_z:0')[:, :, 0]
+
+    start = time.time()
+    _G_z = sess.run([G_z], {z: _z})
+    print('Finished! (Took {} seconds)'.format(time.time() - start))
+
+    os.makedirs(f'infer/{args.checkpoint_name}/', exist_ok=True)
+    
+
+    for i in range(args.samples):
+        if args.human_friendly:
+            raw_sample = np.stack([_G_z[0][i], _G_z[0][i], _G_z[0][i], _G_z[0][i]]).flatten()
+            audio = librosa.effects.time_stretch(raw_sample, 130 / 120)
+        else:
+            audio = _G_z[0][i]
+        lo.write_wav(f'infer/{args.checkpoint_name}/{i}.wav', audio, 16000, norm=False)
+finally:
+    sess.close()
+
+
+# %%
